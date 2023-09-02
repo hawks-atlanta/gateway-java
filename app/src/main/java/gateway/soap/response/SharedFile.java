085fcb0d0de404dcba05702d9820da1cdf3d6563
woynert <whayner.porras.2020@upb.edu.co>
2023-09-02 07:32:10 -0500

    feat: DRY SharedFile class definition

--- app/src/main/java/gateway/soap/response/SharedFile.java
+++ app/src/main/java/gateway/soap/response/SharedFile.java
@@ -1,12 +1,6 @@
 package gateway.soap.response;
 
-import java.util.UUID;
-
-public class SharedFile
+public class SharedFile extends File
 {
-	public String name;
-	public UUID fileUUID;
 	public String ownerUsername;
-	public Boolean is_file;
-	public int size;
 }

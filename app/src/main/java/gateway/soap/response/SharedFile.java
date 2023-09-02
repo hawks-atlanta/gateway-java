58c83436aaaf92e75294a334f2222d998232dbe5
Woynert <61242172+Woynert@users.noreply.github.com>
2023-09-02 07:35:52 -0500

    fox: SOAP share response types (#19)
    * fix: Add file name to SharedFile.java
    
    * fix: Use username instead of userUUID
    
    * feat: DRY SharedFile class definition

--- app/src/main/java/gateway/soap/response/SharedFile.java
+++ app/src/main/java/gateway/soap/response/SharedFile.java
@@ -1,11 +1,6 @@
 package gateway.soap.response;
 
-import java.util.UUID;
-
-public class SharedFile
+public class SharedFile extends File
 {
-	public UUID fileUUID;
 	public String ownerUsername;
-	public Boolean is_file;
-	public int size;
 }

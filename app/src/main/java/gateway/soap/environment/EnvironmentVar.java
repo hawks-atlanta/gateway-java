b3c61dc72118f9660d9a2b9e24604b12d228b2ca
SilviaPabon <silvia.pabon.2020@upb.edu.co>
2023-09-15 19:08:42 -0500

    fix: changing url format

--- app/src/main/java/gateway/soap/environment/EnvironmentVar.java
+++ app/src/main/java/gateway/soap/environment/EnvironmentVar.java
@@ -1,7 +1,5 @@
 package gateway.soap.environment;
 
-public class EnvironmentVar
-{
-	public static final String AUTH_BASEURL =
-		System.getenv ().getOrDefault ("AUTH_BASEURL", "http://localhost:8081/");
+public class EnvironmentVar {
+	public static final String AUTH_BASEURL = System.getenv().getOrDefault("AUTH_BASEURL", "http://localhost:8081");
 }

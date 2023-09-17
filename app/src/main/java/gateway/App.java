8d4e03b06314ca9d0e1be0683ef5042ec0f05f18
woynert <woynertgamer@gmail.com>
2023-09-17 15:43:08 -0500

    chore: Remove RMI demo from App.java

--- app/src/main/java/gateway/App.java
+++ app/src/main/java/gateway/App.java
@@ -16,33 +16,8 @@ public class App
 		// init config from env vars
 		Config.initializeFromEnv ();
 
-		// consume RMI
-		try {
-			// get service
-
-			System.out.println ("Gateway: RMI demo");
-			Registry registry =
-				LocateRegistry.getRegistry (Config.getWorkerHost (), Config.getWorkerPort ());
-			IWorkerService server = (IWorkerService)registry.lookup ("WorkerService");
-
-			// TODO: Replace me
-			// example
-
-			File queryUpload = new File ("----", null);
-			server.uploadFile (queryUpload);
-
-			FileDownload queryDownload = new FileDownload ("----");
-			File resultFile = server.downloadFile (queryDownload);
-
-			System.out.println ("Successfully communicated with worker RMI");
-
-		} catch (Exception e) {
-			System.err.println (e);
-		}
-
-		System.out.println ("Gateway: Starting SOAP");
-
 		// serve SOAP
+		System.out.println ("Gateway: Starting SOAP");
 		Endpoint.publish ("http://0.0.0.0:8080/service", new ServiceImp ());
 	}
 }

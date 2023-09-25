907a538c760f7b4f807a710e0d04ceaf3810bc87
woynert <whayner.porras.2020@upb.edu.co>
2023-09-25 02:10:19 -0500

    feat: Add ResStatus generic downcast support

--- app/src/main/java/gateway/soap/response/ResStatus.java
+++ app/src/main/java/gateway/soap/response/ResStatus.java
@@ -5,4 +5,17 @@ public class ResStatus
 	public int code = 500;
 	public boolean error = true;
 	public String msg = "";
+
+	public static <T extends ResStatus> T downCast (Class<T> c, ResStatus superclass)
+	{
+		try {
+			T subclass = c.getDeclaredConstructor ().newInstance ();
+			subclass.code = superclass.code;
+			subclass.error = superclass.error;
+			subclass.msg = superclass.msg;
+			return subclass;
+		} catch (Exception e) {
+			throw new RuntimeException (e);
+		}
+	}
 }

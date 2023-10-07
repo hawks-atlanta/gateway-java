113eb21eb819b97d0282b7a98767cff6f56df5ec
SilviaPabon <silvia.pabon.2020@upb.edu.co>
2023-10-07 09:42:50 -0500

    fix: adding requested changes in code and tests

--- app/src/main/java/gateway/controller/CtrlAccountRegister.java
+++ app/src/main/java/gateway/controller/CtrlAccountRegister.java
@@ -1,9 +1,9 @@
 package gateway.controller;
 
 import gateway.config.Config;
+import gateway.services.UtilValidator;
 import gateway.soap.request.*;
 import gateway.soap.response.*;
-import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
@@ -11,47 +11,53 @@ import java.net.http.HttpRequest.BodyPublishers;
 import java.net.http.HttpResponse;
 import org.json.JSONObject;
 
-public class CtrlAccountRegister
-{
+public class CtrlAccountRegister {
 	// user register in auth service, returns an access token
-	public static ResSession account_register (Credentials credentials)
-	{
-		ResSession res = new ResSession ();
+	public static ResSession account_register(Credentials credentials) {
+		ResSession res = new ResSession();
 
-		String url = Config.getAuthBaseUrl () + "/register";
+		// validations of all fields
+		ResStatus resValidate = UtilValidator.validate(credentials);
+		if (resValidate.error) {
+			return ResStatus.downCast(ResSession.class, resValidate);
+		}
+
+		String url = Config.getAuthBaseUrl() + "/register";
 
 		// Request
-		JSONObject requestBody = new JSONObject ();
-		requestBody.put ("username", credentials.username);
-		requestBody.put ("password", credentials.password);
+		JSONObject requestBody = new JSONObject();
+		requestBody.put("username", credentials.username);
+		requestBody.put("password", credentials.password);
 
 		try {
 			// Configs and make HTTP POST request to user register.
-
-			HttpResponse<String> response = HttpClient.newHttpClient ().send (
-				HttpRequest.newBuilder ()
-					.uri (URI.create (url))
-					.POST (BodyPublishers.ofString (requestBody.toString ()))
-					.uri (URI.create (url))
-					.header ("Content-Type", "application/json")
-					.build (),
-				HttpResponse.BodyHandlers.ofString ());
+			HttpResponse<String> response = HttpClient.newHttpClient().send(
+					HttpRequest.newBuilder()
+							.uri(URI.create(url))
+							.POST(BodyPublishers.ofString(requestBody.toString()))
+							.header("Content-Type", "application/json")
+							.build(),
+					HttpResponse.BodyHandlers.ofString());
 
 			// Response
-			JSONObject jsonObject = new JSONObject (response.body ());
-			res.code = response.statusCode ();
+			JSONObject jsonObject = new JSONObject(response.body());
+			res.code = response.statusCode();
 
 			if (res.code == 201) {
-				res.auth = new Authorization ();
+				res.auth = new Authorization();
+				res.auth.token = jsonObject.getString("jwt");
 				res.error = false;
-				res.auth.token = jsonObject.getString ("jwt");
+				res.msg = "Register succeed";
 			} else {
 				res.error = true;
-				res.msg = jsonObject.getString ("msg");
+				res.msg = jsonObject.getString("msg");
 			}
 
-		} catch (IOException | InterruptedException e) {
-			e.printStackTrace ();
+		} catch (Exception e) {
+			e.printStackTrace();
+			res.code = 500;
+			res.error = true;
+			res.msg = "Internal error, try again later";
 		}
 
 		return res;

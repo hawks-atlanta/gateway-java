6bee7bb404ce7395dcb5c84563d35735e4adf740
SilviaPabon <silvia.pabon.2020@upb.edu.co>
2023-10-10 14:37:05 -0500

    fix: format fix

--- app/src/main/java/gateway/soap/request/ReqFileMove.java
+++ app/src/main/java/gateway/soap/request/ReqFileMove.java
@@ -1,12 +1,11 @@
 package gateway.soap.request;
 
+import jakarta.validation.constraints.NotNull;
 import java.util.UUID;
 
-import jakarta.validation.constraints.NotNull;
-
-public class ReqFileMove extends Authorization {
-	@NotNull
-	public UUID fileUUID;
+public class ReqFileMove extends Authorization
+{
+	@NotNull public UUID fileUUID;
 	public UUID targetDirectoryUUID; // always a directory
-	public String newName; // optional
+	public String newName;			 // optional
 }

36655348100b3490e831731262a8059e40c5206c
SilviaPabon <silvia.pabon.2020@upb.edu.co>
2023-10-10 14:36:36 -0500

    feat: advance in feat file move

--- app/src/main/java/gateway/controller/CtrlFileMove.java
+++ app/src/main/java/gateway/controller/CtrlFileMove.java
@@ -1,26 +1,70 @@
 package gateway.controller;
 
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.UUID;
+import java.net.http.HttpRequest.BodyPublishers;
+
+import org.json.JSONObject;
+
+import gateway.config.Config;
 import gateway.services.ServiceAuth;
 import gateway.services.UtilValidator;
 import gateway.soap.request.ReqFileMove;
 import gateway.soap.response.ResStatus;
 
-public class CtrlFileMove
-{
-	public static ResStatus file_move (ReqFileMove args)
-	{
-		ResStatus statusRes = new ResStatus ();
+public class CtrlFileMove {
+	public static ResStatus file_move(ReqFileMove args) {
+		ResStatus statusRes = new ResStatus();
 
 		// validation all fields
-		ResStatus resValidate = UtilValidator.validate (args);
+		ResStatus resValidate = UtilValidator.validate(args);
 		if (resValidate.error) {
-			return ResStatus.downCast (ResStatus.class, resValidate);
+			return ResStatus.downCast(ResStatus.class, resValidate);
 		}
 
 		// validation auth
-		ResStatus resAuth = ServiceAuth.authenticate (args.token);
+		ResStatus resAuth = ServiceAuth.authenticate(args.token);
 		if (resAuth.error) {
-			return ResStatus.downCast (ResStatus.class, resAuth);
+			return ResStatus.downCast(ResStatus.class, resAuth);
+		}
+
+		// obtain uuid from user
+		UUID userUUID = UUID.fromString(ServiceAuth.tokenGetClaim(args.token, "uuid"));
+		// request to move file
+		JSONObject requestBody = new JSONObject();
+		requestBody.put("parentUUID", args.targetDirectoryUUID);
+
+		String url = Config.getMetadataBaseUrl() + "/files/move/" + userUUID + "/" + args.fileUUID;
+
+		try {
+
+			HttpClient client = HttpClient.newHttpClient();
+			HttpRequest request = HttpRequest.newBuilder()
+					.uri(URI.create(url))
+					.PUT(BodyPublishers.ofString(requestBody.toString()))
+					.header("Content-Type", "application/json")
+					.build();
+
+			// Response
+			HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+			statusRes.code = response.statusCode();
+
+			if (statusRes.code == 204) {
+				statusRes.error = false;
+				statusRes.msg = "The file have been moved";
+			} else {
+				JSONObject responseBody = new JSONObject(response.body());
+				statusRes.error = true;
+				statusRes.msg = responseBody.getString("message");
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			statusRes.code = 500;
+			statusRes.error = true;
+			statusRes.msg = "Internal error, try again later";
 		}
 
 		return statusRes;
--- app/src/main/java/gateway/soap/request/ReqFileMove.java
+++ app/src/main/java/gateway/soap/request/ReqFileMove.java
@@ -2,9 +2,11 @@ package gateway.soap.request;
 
 import java.util.UUID;
 
-public class ReqFileMove extends Authorization
-{
+import jakarta.validation.constraints.NotNull;
+
+public class ReqFileMove extends Authorization {
+	@NotNull
 	public UUID fileUUID;
 	public UUID targetDirectoryUUID; // always a directory
-	public String newName;			 // optional
+	public String newName; // optional
 }

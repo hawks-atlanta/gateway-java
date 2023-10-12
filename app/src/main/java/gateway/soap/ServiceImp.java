0aec012e815471d065c5e172ae72c47a74a4b902
Andres David Bonilla Higuera <andres.bonilla.2020@upb.edu.co>
2023-10-12 08:57:11 -0500

    Update password (#68)
    * Advance
    
    * Finished Update Password
    
    * Fix: Format
    
    * Fix: Format sh
    
    * Fixs from CtrlUpdatePassword

--- app/src/main/java/gateway/soap/ServiceImp.java
+++ app/src/main/java/gateway/soap/ServiceImp.java
@@ -27,7 +27,10 @@ import javax.jws.WebService;
 		return CtrlAccountRegister.account_register (credentials);
 	}
 
-	@WebMethod public ResStatus account_password (ReqAccPassword parameters) { return null; }
+	@WebMethod public ResStatus account_password (ReqAccPassword parameters)
+	{
+		return CtrlUpdatePassword.account_password (parameters);
+	}
 
 	// file system
 

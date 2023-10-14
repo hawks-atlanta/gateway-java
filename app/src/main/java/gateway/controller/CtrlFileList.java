9d7f099553c848061247cb0eac9c7f6ae0419de9
andres123dbh <60190072+andres123dbh@users.noreply.github.com>
2023-10-13 21:25:29 -0500

    Fix Format

--- app/src/main/java/gateway/controller/CtrlFileList.java
+++ app/src/main/java/gateway/controller/CtrlFileList.java
@@ -18,32 +18,32 @@ public class CtrlFileList
 {
 	public static ResFileList file_list (ReqFileList args)
 	{
-        //Create a new instance of ResFileList to store the results
+		// Create a new instance of ResFileList to store the results
 		ResFileList resFileList = new ResFileList ();
 		UUID userUUID;
 		String url;
 
-		//User authentication
+		// User authentication
 		ResStatus resAuth = ServiceAuth.authenticate (args.token);
 		if (resAuth.error) {
 			return ResStatus.downCast (ResFileList.class, resAuth);
 		}
 
-        //Get the user's UUID from the authentication token
+		// Get the user's UUID from the authentication token
 		userUUID = UUID.fromString (ServiceAuth.tokenGetClaim (args.token, "uuid"));
 
-        //Build the URL for the request.
+		// Build the URL for the request.
 		if (args.location == null || args.location.equals (new UUID (0L, 0L))) {
-            //If no location is specified, build the URL without a parentUUID parameter
+			// If no location is specified, build the URL without a parentUUID parameter
 			url = Config.getMetadataBaseUrl () + "/files/list/" + userUUID;
 		} else {
-            //If a location is specified, include the parentUUID parameter
+			// If a location is specified, include the parentUUID parameter
 			url = Config.getMetadataBaseUrl () + "/files/list/" + userUUID +
 				  "?parentUUID=" + args.location;
 		}
 
 		try {
-            //Make an HTTP GET request
+			// Make an HTTP GET request
 			HttpResponse<String> response = HttpClient.newHttpClient ().send (
 				HttpRequest.newBuilder ().uri (URI.create (url)).GET ().build (),
 				HttpResponse.BodyHandlers.ofString ());
@@ -52,22 +52,22 @@ public class CtrlFileList
 			resFileList.code = response.statusCode ();
 
 			if (resFileList.code == 200) {
-                //If the response code is 200, process the received files
+				// If the response code is 200, process the received files
 				JSONArray filesArray = responseBody.getJSONArray ("files");
 				File[] files = new File[filesArray.length ()];
 
 				for (int i = 0; i < filesArray.length (); i++) {
-                    //Process each file in the response JSON
+					// Process each file in the response JSON
 					JSONObject fileObject = filesArray.getJSONObject (i);
 
-                    //Extract file information
+					// Extract file information
 					String fileuuid = fileObject.getString ("uuid");
 					String fileType = fileObject.getString ("fileType");
 					String fileName = fileObject.getString ("name");
 					String fileExtension =
 						fileObject.isNull ("extension") ? null : fileObject.getString ("extension");
 
-                    //Create a File object and assign it the file information
+					// Create a File object and assign it the file information
 					File file = new File ();
 
 					file.uuid = UUID.fromString (fileuuid);
@@ -82,12 +82,13 @@ public class CtrlFileList
 				resFileList.error = false;
 				resFileList.msg = "Files listed successfully";
 			} else {
-                //If the response code is not 200, set error and error message based on the response
+				// If the response code is not 200, set error and error message based on the
+				// response
 				resFileList.error = true;
 				resFileList.msg = responseBody.getString ("message");
 			}
 		} catch (Exception e) {
-            //In case of an exception
+			// In case of an exception
 			e.printStackTrace ();
 			resFileList.code = 500;
 			resFileList.error = true;

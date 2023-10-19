8bb0638b21792503dd6d21eab0c4d6c6136f4daf
SilviaPabon <silvia.pabon.2020@upb.edu.co>
2023-10-19 09:41:10 -0500

    fix: format

--- app/src/main/java/gateway/controller/CtrlFileList.java
+++ app/src/main/java/gateway/controller/CtrlFileList.java
@@ -15,45 +15,47 @@ import java.util.UUID;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
-public class CtrlFileList {
-	public static ResFileList file_list(ReqFileList args) {
+public class CtrlFileList
+{
+	public static ResFileList file_list (ReqFileList args)
+	{
 		// Create a new instance of ResFileList to store the results
-		ResFileList resFileList = new ResFileList();
+		ResFileList resFileList = new ResFileList ();
 		UUID userUUID;
 		String url;
 
 		// User authentication
-		ResStatus resAuth = ServiceAuth.authenticate(args.token);
+		ResStatus resAuth = ServiceAuth.authenticate (args.token);
 		if (resAuth.error) {
-			return ResStatus.downCast(ResFileList.class, resAuth);
+			return ResStatus.downCast (ResFileList.class, resAuth);
 		}
 
 		// Get the user's UUID from the authentication token
-		userUUID = UUID.fromString(ServiceAuth.tokenGetClaim(args.token, "uuid"));
+		userUUID = UUID.fromString (ServiceAuth.tokenGetClaim (args.token, "uuid"));
 
 		// Build the URL for the request.
 		if (args.location == null) {
 			// If no location is specified, build the URL without a parentUUID parameter
-			url = Config.getMetadataBaseUrl() + "/files/list/" + userUUID;
+			url = Config.getMetadataBaseUrl () + "/files/list/" + userUUID;
 		} else {
 			// If a location is specified, include the parentUUID parameter
-			url = Config.getMetadataBaseUrl() + "/files/list/" + userUUID +
-					"?parentUUID=" + args.location;
+			url = Config.getMetadataBaseUrl () + "/files/list/" + userUUID +
+				  "?parentUUID=" + args.location;
 		}
 
 		try {
 			// Make an HTTP GET request
-			HttpResponse<String> response = HttpClient.newHttpClient().send(
-					HttpRequest.newBuilder().uri(URI.create(url)).GET().build(),
-					HttpResponse.BodyHandlers.ofString());
+			HttpResponse<String> response = HttpClient.newHttpClient ().send (
+				HttpRequest.newBuilder ().uri (URI.create (url)).GET ().build (),
+				HttpResponse.BodyHandlers.ofString ());
 
-			JSONObject responseBody = new JSONObject(response.body());
-			resFileList.code = response.statusCode();
+			JSONObject responseBody = new JSONObject (response.body ());
+			resFileList.code = response.statusCode ();
 
 			if (resFileList.code == 200) {
 				// If the response code is 200, process the received files
-				JSONArray filesArray = responseBody.getJSONArray("files");
-				File[] files = UtilsFiles.createFileArray(filesArray);
+				JSONArray filesArray = responseBody.getJSONArray ("files");
+				File[] files = UtilsFiles.createFileArray (filesArray);
 
 				resFileList.files = files;
 				resFileList.error = false;
@@ -62,11 +64,11 @@ public class CtrlFileList {
 				// If the response code is not 200, set error and error message based on the
 				// response
 				resFileList.error = true;
-				resFileList.msg = responseBody.getString("message");
+				resFileList.msg = responseBody.getString ("message");
 			}
 		} catch (Exception e) {
 			// In case of an exception
-			e.printStackTrace();
+			e.printStackTrace ();
 			resFileList.code = 500;
 			resFileList.error = true;
 			resFileList.msg = "Internal error, try again later";

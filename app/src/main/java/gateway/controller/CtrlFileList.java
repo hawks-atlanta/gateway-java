ce8a57322885762a33030f1e19d8c374f09a9d97
SilviaPabon <silvia.pabon.2020@upb.edu.co>
2023-10-19 09:40:47 -0500

    fix: updating file of services where is createFileArray method

--- app/src/main/java/gateway/controller/CtrlFileList.java
+++ app/src/main/java/gateway/controller/CtrlFileList.java
@@ -2,7 +2,7 @@ package gateway.controller;
 
 import gateway.config.Config;
 import gateway.services.ServiceAuth;
-import gateway.services.ServiceMetadata;
+import gateway.services.UtilsFiles;
 import gateway.soap.request.ReqFileList;
 import gateway.soap.response.File;
 import gateway.soap.response.ResFileList;
@@ -15,47 +15,45 @@ import java.util.UUID;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
-public class CtrlFileList
-{
-	public static ResFileList file_list (ReqFileList args)
-	{
+public class CtrlFileList {
+	public static ResFileList file_list(ReqFileList args) {
 		// Create a new instance of ResFileList to store the results
-		ResFileList resFileList = new ResFileList ();
+		ResFileList resFileList = new ResFileList();
 		UUID userUUID;
 		String url;
 
 		// User authentication
-		ResStatus resAuth = ServiceAuth.authenticate (args.token);
+		ResStatus resAuth = ServiceAuth.authenticate(args.token);
 		if (resAuth.error) {
-			return ResStatus.downCast (ResFileList.class, resAuth);
+			return ResStatus.downCast(ResFileList.class, resAuth);
 		}
 
 		// Get the user's UUID from the authentication token
-		userUUID = UUID.fromString (ServiceAuth.tokenGetClaim (args.token, "uuid"));
+		userUUID = UUID.fromString(ServiceAuth.tokenGetClaim(args.token, "uuid"));
 
 		// Build the URL for the request.
 		if (args.location == null) {
 			// If no location is specified, build the URL without a parentUUID parameter
-			url = Config.getMetadataBaseUrl () + "/files/list/" + userUUID;
+			url = Config.getMetadataBaseUrl() + "/files/list/" + userUUID;
 		} else {
 			// If a location is specified, include the parentUUID parameter
-			url = Config.getMetadataBaseUrl () + "/files/list/" + userUUID +
-				  "?parentUUID=" + args.location;
+			url = Config.getMetadataBaseUrl() + "/files/list/" + userUUID +
+					"?parentUUID=" + args.location;
 		}
 
 		try {
 			// Make an HTTP GET request
-			HttpResponse<String> response = HttpClient.newHttpClient ().send (
-				HttpRequest.newBuilder ().uri (URI.create (url)).GET ().build (),
-				HttpResponse.BodyHandlers.ofString ());
+			HttpResponse<String> response = HttpClient.newHttpClient().send(
+					HttpRequest.newBuilder().uri(URI.create(url)).GET().build(),
+					HttpResponse.BodyHandlers.ofString());
 
-			JSONObject responseBody = new JSONObject (response.body ());
-			resFileList.code = response.statusCode ();
+			JSONObject responseBody = new JSONObject(response.body());
+			resFileList.code = response.statusCode();
 
 			if (resFileList.code == 200) {
 				// If the response code is 200, process the received files
-				JSONArray filesArray = responseBody.getJSONArray ("files");
-				File[] files = ServiceMetadata.createFileArray (filesArray);
+				JSONArray filesArray = responseBody.getJSONArray("files");
+				File[] files = UtilsFiles.createFileArray(filesArray);
 
 				resFileList.files = files;
 				resFileList.error = false;
@@ -64,11 +62,11 @@ public class CtrlFileList
 				// If the response code is not 200, set error and error message based on the
 				// response
 				resFileList.error = true;
-				resFileList.msg = responseBody.getString ("message");
+				resFileList.msg = responseBody.getString("message");
 			}
 		} catch (Exception e) {
 			// In case of an exception
-			e.printStackTrace ();
+			e.printStackTrace();
 			resFileList.code = 500;
 			resFileList.error = true;
 			resFileList.msg = "Internal error, try again later";
--- app/src/main/java/gateway/controller/CtrlShareList.java
+++ app/src/main/java/gateway/controller/CtrlShareList.java
@@ -2,7 +2,7 @@ package gateway.controller;
 
 import gateway.config.Config;
 import gateway.services.ServiceAuth;
-import gateway.services.ServiceMetadata;
+import gateway.services.UtilsFiles;
 import gateway.soap.request.Authorization;
 import gateway.soap.response.File;
 import gateway.soap.response.ResShareList;
@@ -14,46 +14,44 @@ import java.util.UUID;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
-public class CtrlShareList
-{
-	public static ResShareList share_list (Authorization authorization)
-	{
-		ResShareList resShareList = new ResShareList ();
+public class CtrlShareList {
+	public static ResShareList share_list(Authorization authorization) {
+		ResShareList resShareList = new ResShareList();
 
 		// obtain uuid from user
-		UUID userUUID = UUID.fromString (ServiceAuth.tokenGetClaim (authorization.token, "uuid"));
+		UUID userUUID = UUID.fromString(ServiceAuth.tokenGetClaim(authorization.token, "uuid"));
 
-		String url = Config.getMetadataBaseUrl () + "/files/shared_with_me/" + userUUID;
+		String url = Config.getMetadataBaseUrl() + "/files/shared_with_me/" + userUUID;
 
 		try {
 
-			HttpResponse<String> response = HttpClient.newHttpClient ().send (
-				HttpRequest.newBuilder ()
-					.uri (URI.create (url))
-					.GET ()
-					.header ("Authorization", "Bearer " + authorization.token)
-					.build (),
-				HttpResponse.BodyHandlers.ofString ());
+			HttpResponse<String> response = HttpClient.newHttpClient().send(
+					HttpRequest.newBuilder()
+							.uri(URI.create(url))
+							.GET()
+							.header("Authorization", "Bearer " + authorization.token)
+							.build(),
+					HttpResponse.BodyHandlers.ofString());
 
 			// Response
-			JSONObject responseBody = new JSONObject (response.body ());
-			resShareList.code = response.statusCode ();
+			JSONObject responseBody = new JSONObject(response.body());
+			resShareList.code = response.statusCode();
 
 			if (resShareList.code == 200) {
 				// If the response code is 200, process the received files
-				JSONArray shareFilesArray = responseBody.getJSONArray ("files");
-				File[] shareFiles = ServiceMetadata.createFileArray (shareFilesArray);
+				JSONArray shareFilesArray = responseBody.getJSONArray("files");
+				File[] shareFiles = UtilsFiles.createFileArray(shareFilesArray);
 
 				resShareList.error = false;
 				resShareList.sharedFiles = shareFiles;
 				resShareList.msg = "Ok. The directory was listed.";
 			} else {
 				resShareList.error = true;
-				resShareList.msg = responseBody.getString ("message");
+				resShareList.msg = responseBody.getString("message");
 			}
 
 		} catch (Exception e) {
-			e.printStackTrace ();
+			e.printStackTrace();
 			resShareList.code = 500;
 			resShareList.error = true;
 			resShareList.msg = "Internal error, try again later";

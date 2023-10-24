1c24ccf5ff1446acd40ec31b2bf009a944e8a0af
SilviaPabon <silvia.pabon.2020@upb.edu.co>
2023-10-24 01:10:16 -0500

    feat: unshare file feat init

--- app/src/main/java/gateway/soap/request/ReqShareRemove.java
+++ app/src/main/java/gateway/soap/request/ReqShareRemove.java
@@ -4,8 +4,15 @@ import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import java.util.UUID;
 
-public class ReqShareRemove
-{
-	@NotNull public UUID fileUUID;
-	@NotEmpty public String otherUsername;
+public class ReqShareRemove extends Authorization {
+	@NotNull
+	public UUID fileUUID;
+	@NotEmpty
+	public String otherUsername;
+	
+	public ReqShareRemove(String token, @NotNull UUID fileUUID, @NotEmpty String otherUsername) {
+		super(token);
+		this.fileUUID = fileUUID;
+		this.otherUsername = otherUsername;
+	}
 }

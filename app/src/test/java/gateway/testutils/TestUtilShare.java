360432081a7ee3ff3b3e14b0667804fbaccfc685
Silvia Pabón <62608580+SilviaPabon@users.noreply.github.com>
2023-10-24 16:51:28 -0500

    feat: unshare file (#86)
    * feat: unshare file feat init
    
    * fix: format
    
    * fix: fix in some names
    
    * fix: format
    
    * fix: format
    
    * feat: add createShareRemove object util
    
    * feat: unshare file feat and tests done

--- app/src/test/java/gateway/testutils/TestUtilShare.java
+++ app/src/test/java/gateway/testutils/TestUtilShare.java
@@ -2,6 +2,7 @@ package gateway.testutils;
 
 import gateway.services.ServiceMetadata.ResSaveFile;
 import gateway.soap.request.ReqShareFile;
+import gateway.soap.request.ReqShareRemove;
 
 public class TestUtilShare
 {
@@ -9,6 +10,16 @@ public class TestUtilShare
 	createShareFile (ResSaveFile resSaveFile, String otherUsername, String token)
 	{
 		ReqShareFile reqShareFile = new ReqShareFile ();
+		reqShareFile.fileUUID = resSaveFile.fileUUID;
+		reqShareFile.otherUsername = otherUsername;
+		reqShareFile.token = token;
+		return reqShareFile;
+	}
+
+	public static ReqShareRemove
+	createShareRemove (ResSaveFile resSaveFile, String otherUsername, String token)
+	{
+		ReqShareRemove reqShareFile = new ReqShareRemove ();
 		reqShareFile.fileUUID = resSaveFile.fileUUID;
 		reqShareFile.otherUsername = otherUsername;
 		reqShareFile.token = token;
